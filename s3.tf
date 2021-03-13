1eb5d53961cc57098389f6266d094fe27e3bcd2b
Simon Adediran <simon.adediran@axoni.com>
2021-03-13 15:41:50 -0600

    Test 3

--- s3.tf
+++ s3.tf
@@ -6,22 +6,3 @@ resource "aws_s3_bucket" "simonhyclouds3" {
   }
 
 }
-
-
-resource "aws_s3_bucket" "simonhyclouds4" {
-  bucket = "simonhyloud097"
-  acl = "private"
-  tags = {
-    Department = "HR"
-  }
-
-}
-
-resource "aws_s3_bucket" "simonhyclouds5" {
-  bucket = "simonhyloud0997"
-  acl = "private"
-  tags = {
-    Department = "HR"
-  }
-
-}
